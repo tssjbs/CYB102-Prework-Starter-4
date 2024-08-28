bf31860155e168da41b286d376ec0ee2e360fa32
tssjbs <jeanrussosaus@gmail.com>
2024-08-28 04:07:03 +0000

    Initial commit

---
+++ prework.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+#####
+# Welcome to the CYB102 Prework bash script!
+#
+# Prework Instructions: 
+# https://courses.codepath.org/snippets/cyb102-dev/prework
+# 
+# Your mission: write linux terminal commands to perform a series of tasks.
+# 
+# Click "Run" to check your work.
+#    Warning: When you Run the check tool, it will RESET the .txt files 
+#    to their default states.  Editing them manually is not enough -- 
+#    this script MUST edit them for you!
+#####
+
+###
+# Challenge #1: Hello, my name is...
+###
+# GOAL: Replace the contents of the "intro.txt" file with an introduction!
+# COMMAND: echo (https://linux.die.net/man/1/echo) 
+###
+echo "Replace this line with your answer to challenge #1"
+
+###
+# Challenge #2: Digital sharpie
+###
+# GOAL: Make the file "permanent.txt" unable to be edited (AKA written).
+# COMMAND: chmod (https://linux.die.net/man/1/chmod) 
+###
+echo "Replace this line with your answer to challenge #2"
+
+###
+# Challenge #3: Backup your backups!
+###
+# GOAL: Make an exact copy of the file "random.txt" called "copy.txt"
+# COMMAND: cp (https://linux.die.net/man/1/cp) 
+###
+echo "Replace this line with your answer to challenge #3"
